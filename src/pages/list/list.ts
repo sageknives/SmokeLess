9b08b3cc986cbc4ba6b79e65903e7ff2a5c1e659
sageknives <sageknives@gmail.com>
2018-02-28 10:28:27 -0800

    added pagination to list

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
+import * as moment from 'moment';
 import { Entry } from '../../models/smoke/entry';
 import { SmokingService } from '../../providers/smoking-service';
 import { UserService } from '../../providers/user-service';
@@ -13,59 +14,80 @@ import { ToastService } from '../../providers/toast-service';
 })
 export class ListPage {
   private title = "List View";
-  private user:User;
+  private user: User;
   private entries = new Array<Entry>();
+  selectedDate: string = moment(Date.now()).startOf('day').toISOString();
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private userService: UserService,
-    private smokingService:SmokingService,
-    private toast:ToastService,
+    private smokingService: SmokingService,
+    private toast: ToastService,
     public appCtrl: App
 
   ) {
-    // for (let i = 0; i < 25; i++) {
-    //   let numString = i>9?i:"0"+i;
-    //   this.entries.push(new Entry(undefined,"2018-02-"+numString+":00:00Z",1,undefined));
-    // }
   }
 
   ionViewDidLoad() {
-    
+
   }
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.refresh();
   }
-  refresh(){
+  refresh() {
     this.userService.getLoggedInUser()
-    .then((user:User)=>{
-      this.user = user;
-      if (!this.user) {
-        this.navCtrl.setRoot('LoginPage');
-        this.toast.show("Please login to continue");
-        return;
-      }else{
-        return this.smokingService.getAllEntries(this.user.getId());
-      }
-    }).then((entries:Entry[])=>{
-      this.entries = entries.sort((a,b)=>{
-        return a.getStart() > b.getStart()? -1 : a.getStart() < b.getStart() ? 1 : 0;
-      });
-    }).catch(this.toast.showError);
+      .then((user: User) => {
+        this.user = user;
+        if (!this.user) {
+          this.navCtrl.setRoot('LoginPage');
+          this.toast.show("Please login to continue");
+          return;
+        } else {
+          this.selectedDate = moment(Date.now()).startOf('day').toISOString();
+          return this.addDay();
+        }
+      }).then((entries: Entry[]) => {
+        this.entries = entries.sort((a, b) => {
+          return a.getStart() > b.getStart() ? -1 : a.getStart() < b.getStart() ? 1 : 0;
+        });
+      }).catch(this.toast.showError);
+  }
+
+  addDay(): Promise<Entry[]> {
+    return new Promise((resolve, reject) => {
+      let start = moment(this.selectedDate).startOf('day').toISOString();
+      let end = moment(start).add(1, 'day').toISOString();
+      this.smokingService.getEntries(this.user.getId(), start, end)
+        .then((entries: Entry[]) => {
+          resolve(entries);
+        }).then(reject);
+    })
   }
 
   goToEntry(entry: Entry) {
-    this.navCtrl.push('EntryPage',{id:entry.getId()});
+    this.navCtrl.push('EntryPage', { id: entry.getId() });
   }
 
-  addEntry(){
+  addEntry() {
     this.navCtrl.push('EntryPage');
   }
 
-  gotoAnalytics(){
+  gotoAnalytics() {
     this.appCtrl.getRootNav().push('GraphTabsPage')
+  }
 
-    //this.navCtrl.parent.parent.push('GraphTabsPage');
+  getMore() {
+    this.selectedDate = moment(this.selectedDate).add(-1, 'day').startOf('day').toISOString();
+    this.addDay()
+      .then((entries: Entry[]) => {
+        this.entries = this.entries.concat(entries).sort((a, b) => {
+          return a.getStart() > b.getStart() ? -1 : a.getStart() < b.getStart() ? 1 : 0;
+        });
+      }).catch(error => {
+        this.toast.show("Error getting previous day");
+      })
+
   }
 
 }
